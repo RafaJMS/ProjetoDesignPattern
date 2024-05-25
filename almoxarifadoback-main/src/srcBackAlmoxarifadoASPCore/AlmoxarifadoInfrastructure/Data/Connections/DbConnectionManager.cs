1f59850bb18a6da73219e3b8e44f0a49ecb4e15b
Rafael José Mecenas Silva <rafaeljmecenas1@gmail.com>
2024-05-25 12:11:21 -0300

    commit entrega 5

---
+++ almoxarifadoback-main/src/srcBackAlmoxarifadoASPCore/AlmoxarifadoInfrastructure/Data/Connections/DbConnectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmoxarifadoInfrastructure.Data.Connections
+{
+    public class DbConnectionManager
+    {
+        private readonly IEnumerable<IDbConnectionStrategy> _strategies;
+
+        public DbConnectionManager(IEnumerable<IDbConnectionStrategy> strategies)
+        {
+            _strategies = strategies;
+        }
+
+        public string GetActiveConnectionString()
+        {
+            foreach (var strategy in _strategies)
+            {
+                try
+                {
+                    return strategy.GetConnectionString();
+                }
+                catch (InvalidOperationException)
+                {
+                    
+                }
+            }
+            throw new InvalidOperationException("All connection strategies failed.");
+        }
+    }
+}
