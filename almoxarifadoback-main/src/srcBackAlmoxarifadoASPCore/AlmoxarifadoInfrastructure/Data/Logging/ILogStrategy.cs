4c46e8ea06e456ce0b925aba5a7dcf1777a6c519
Rafael José Mecenas Silva <rafaeljmecenas1@gmail.com>
2024-05-25 12:03:36 -0300

    commit entrega 4

---
+++ almoxarifadoback-main/src/srcBackAlmoxarifadoASPCore/AlmoxarifadoInfrastructure/Data/Logging/ILogStrategy.cs
@@ -0,0 +1,14 @@
+﻿using AlmoxarifadoDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmoxarifadoInfrastructure.Data.Logging
+{
+    public interface ILogStrategy
+    {
+        void CriarLog(Estoque estoque, decimal ID_REQ);
+    }
+}
