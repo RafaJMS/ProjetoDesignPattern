8999e6adf621b5221f9398cbcabdce3af17f7aae
Rafael José Mecenas Silva <rafaeljmecenas1@gmail.com>
2024-05-25 11:57:54 -0300

    commit entrega 2 e 3

---
+++ almoxarifadoback-main/src/srcBackAlmoxarifadoASPCore/AlmoxarifadoInfrastructure/Data/Command/EntradaEstoqueCommand.cs
@@ -0,0 +1,31 @@
+﻿using AlmoxarifadoDomain.Models;
+using AlmoxarifadoInfrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmoxarifadoServices.Observer
+{
+    public class EntradaEstoqueCommand : ICommand
+    {
+        private readonly Itens_Nota _itemNota;
+
+        public EntradaEstoqueCommand(Itens_Nota itemNota)
+        {
+            _itemNota = itemNota;
+        }
+
+        public void Execute(ContextSQL context)
+        {
+            var estoque = context.Estoque.FirstOrDefault(e => e.ID_PRO == _itemNota.ID_PRO && e.ID_SEC == _itemNota.ID_SEC);
+            if (estoque != null)
+            {
+                estoque.QTD_PRO += _itemNota.QTD_PRO;
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
---
+++ almoxarifadoback-main/src/srcBackAlmoxarifadoASPCore/AlmoxarifadoInfrastructure/Data/Command/EstoqueService.cs
@@ -0,0 +1,29 @@
+﻿using AlmoxarifadoDomain.Models;
+using AlmoxarifadoInfrastructure.Data;
+using AlmoxarifadoInfrastructure.Data.Interfaces;
+using AlmoxarifadoInfrastructure.Data.Logging;
+using AlmoxarifadoServices.Observer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlmoxarifadoServices.Services
+{
+    public class EstoqueService
+    {
+        private readonly ContextSQL _context;
+
+        public EstoqueService(ContextSQL context)
+        {
+            _context = context;
+        }
+
+        public void ExecutarComando(ICommand command)
+        {
+            command.Execute(_context);
+        }
+    }
+}
+
